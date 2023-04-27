db6f2f740d170972d72465a2a51f8ec10f0b550f
nsoids <z752910357@gmail.com>
2023-04-27 16:01:25 +0800

    Optimize the function using iterator adaptors

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ impl Config {
             Some(arg) => arg,
             None => return Err("Didn't get a filename"),
         };
-        
+
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
 
         Ok(Config {
@@ -46,26 +46,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
